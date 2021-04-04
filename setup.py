37b55a1c14b79ac2d8df6e7a5d058d072de07417
monolith <lbhdc@users.noreply.github.com>
2021-04-04 17:06:55 -0400

    compiler args

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, Extension
 from Cython.Build import build_ext
 sources = ["fmath.pyx", "lib.pxd", "floats.pxd"]
-modules = [Extension("fmath", sources, include_dirs=".", language="c++")]
-setup(cmdclass={'build_ext': build_ext}, ext_modules=modules)
+fmath = Extension("fmath", sources, include_dirs=".", language="c++", extra_compile_args=["-O3"])
+setup(cmdclass={'build_ext': build_ext}, ext_modules=[fmath])
