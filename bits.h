73d4c5260f08aacf6f5ead83af4c3da9294fabfc
monolith <lbhdc@users.noreply.github.com>
2021-04-04 20:32:22 -0400

    masks and merge

--- bits.h
+++ bits.h
@@ -1,7 +1,18 @@
 #ifndef FMATH_BITS_H
 #define FMATH_BITS_H
 
+#include <climits>
+
 namespace fmath {
+template <typename R>
+static constexpr R bitmask(unsigned int const onecount) {
+  return static_cast<R>(-(onecount != 0)) & (static_cast<R>(-1) >> ((sizeof(R) * CHAR_BIT - onecount)));
+}
+
+inline unsigned merge(unsigned int a, unsigned int b, unsigned int mask) {
+  return a ^ ((a ^ b) & mask);
+}
+
 inline unsigned int reverse_bits(unsigned int v) {
   v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
   v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
--- bits_test.cc
+++ bits_test.cc
@@ -2,6 +2,14 @@
 #include "bits.h"
 
 namespace {
+TEST(bits, merge) {
+  auto a = 0b00000000000000000000000000000000;
+  auto b = 0b11111111111111111111111111111111;
+  auto mask = fmath::bitmask<unsigned int>(16);
+  ASSERT_EQ(fmath::merge(a, b, mask), mask);
+
+}
+
 TEST(bits, reverse) {
   ASSERT_EQ(fmath::reverse_bits(0b00000000000000001111111111111111), 0b11111111111111110000000000000000);
 }
