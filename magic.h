6e9702b93604ae7f78b005f9ec4747478bf0f222
monolith <lbhdc@users.noreply.github.com>
2021-04-06 20:26:44 -0400

    normal_ratio and fmt

--- magic.h
+++ magic.h
@@ -2,12 +2,13 @@
 #define FMATH_MAGIC_H
 
 namespace fmath::magic {
-const unsigned int negative_zero_f = 0x80000000;
-const unsigned int one_f =  0x3F800000;
-const float scale_up = static_cast<float>(0x00800000); // 8388608.0, 2^23
-const float scale_down = 1.0f / scale_up;
-const int lomont_magic_num = 0x5f375a86;
-const int quake_magic_num = 0x5f3759df;
+static const int          lomont_magic_num = 0x5f375a86;
+static const unsigned int negative_zero_f  = 0x80000000;
+static const double       normal_ratio     = (1.0 / 18446744073709551616.0);
+static const unsigned int one_f            = 0x3F800000;
+static const int          quake_magic_num  = 0x5f3759df;
+static const float        scale_up         = static_cast<float>(0x00800000); // 8388608.0, 2^23
+static const float        scale_down       = 1.0f / scale_up;
 }
 
 #endif //FMATH_MAGIC_H
