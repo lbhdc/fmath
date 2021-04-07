5c70fc0b55a4ab02924bd88a49b681ca382c6ed6
monolith <lbhdc@users.noreply.github.com>
2021-04-06 20:42:02 -0400

    constexpr lib

--- lib.h
+++ lib.h
@@ -2,14 +2,33 @@
 #define FMATH_LIB_H
 
 #include <climits>
+#include "lib.h"
 
 namespace fmath {
-int abs(int v);
-bool is_pow2(int v);
-int max(int a, int b);
-int min(int a, int b);
-bool same_signs(int a, int b);
-int sign(int v);
+constexpr bool is_pow2(int v) noexcept {
+  return v && !(v & (v - 1));
+}
+
+constexpr int max(int a, int b) noexcept {
+  return a - ((a - b) & ((a - b) >> (sizeof(int) * CHAR_BIT - 1)));
+}
+
+constexpr int min(int a, int b) noexcept {
+  return b + ((a - b) & ((a - b) >> (sizeof(int) * CHAR_BIT - 1)));
+}
+
+constexpr bool same_signs(int a, int b) noexcept {
+  return (a ^ b) >= 0;
+}
+
+constexpr int sign(int v) noexcept {
+  return v >> (sizeof(int) * CHAR_BIT - 1);
+}
+
+constexpr int abs(int v) noexcept {
+  const int mask = sign(v);
+  return (v + mask) ^ mask;
+}
 }
 
 #endif //FMATH_LIB_H
