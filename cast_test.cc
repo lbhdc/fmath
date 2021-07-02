6f0027c9aaff91c1a01d962531ef73bc73fc7470
monolith <lbhdc@users.noreply.github.com>
2021-07-02 15:51:46 -0500

    add cases for missing branches

--- cast_test.cc
+++ cast_test.cc
@@ -3,8 +3,14 @@
 
 namespace {
 TEST(floats, type_conversion) {
-  auto have = 1.0f;
-  auto got = fmath::as_float(fmath::as_int(have));
-  EXPECT_EQ(have, got);
+  // 32bit
+  auto have1 = 1.0f;
+  auto got1 = fmath::as_float(fmath::as_int(have1));
+  EXPECT_EQ(have1, got1);
+
+  // 64bit
+  auto have2 = 1.0;
+  auto got2 = fmath::as_float(fmath::as_int(have2));
+  EXPECT_EQ(have2, got2);
 }
 }
--- numeric_test.cc
+++ numeric_test.cc
@@ -3,6 +3,7 @@
 
 namespace {
 TEST(lib, is_pow2) {
+  ASSERT_FALSE(fmath::is_pow2(0));
   ASSERT_TRUE(fmath::is_pow2(2));
   ASSERT_FALSE(fmath::is_pow2(3));
 }
